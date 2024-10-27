4bf35b27b8904375fe0d73abf957edc88c1cd368
Matheus.Jagi <matheus.jagi@sicoob.com.br>
2024-10-27 19:13:52 -0300

    Fix set universal

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,10 +6,10 @@ def run(): Unit =
     var sets: Map[String, Set[Any]] = Map()
 
     while continue do
-        println("Insira o nome do conjunto de A a Z (ou 'c' para continuar):")
+        println("Insira o nome do conjunto de A a Z (ou ':c' para continuar):")
         val setName = StdIn.readLine().toUpperCase()
 
-        if setName != "C" then
+        if setName != ":C" then
             println(s"Agora insira os valores do conjunto [$setName] separados por virgula (ex.: 1,5,7):")
             val setValues: Set[Any] = StdIn.readLine().split(',').map(_.trim).toSet
 
@@ -27,9 +27,9 @@ def run(): Unit =
 
             println("\nInforme a expressão que deseja calcular para os conjuntos (ex.:  A | (B & C) -> Respeite os espacos eos conjuntos): ")
             val expression = StdIn.readLine()
-            val result = SetParser.parse(expression).evaluate(sets)
+            val result = SetParser.parse(expression).evaluate(sets, sets.values.flatten.toSet)
             println(s"\nO resultado da expressão [$expression] e: $result")
 
-            println("\n\nPara finalizar o programa digite 'q' ou ENTER para continuar")
+            println("\n\nPara finalizar o programa digite ':q' ou ENTER para continuar")
             val quit = StdIn.readLine().toUpperCase()
-            if quit == "Q" then continue = false
+            if quit == ":Q" then continue = false
